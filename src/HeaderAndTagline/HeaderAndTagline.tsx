edd7297ed92838be8bcde83cc3172cb6da8798c9
gevpanagopoulou <gevpanagopoulou@gmail.com>
2025-02-05 21:02:56 +0200

    implement Tagline component

--- src/HeaderAndTagline/HeaderAndTagline.tsx
+++ src/HeaderAndTagline/HeaderAndTagline.tsx
@@ -1,9 +1,13 @@
 import Header from "./HeaderSection/Header";
+import Tagline from "./TaglineSection/Tagline";
 
 function HeaderAndTagline() {
   return (
-    <div>
+    <div
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
       <Header />
+      <Tagline />
     </div>
   );
 }
