13d7ed4b6e08defaa9f7172fa6f0e9eccd45e5d4
gevpanagopoulou <gevpanagopoulou@gmail.com>
2025-02-06 17:38:53 +0200

    fix header and tagline width

--- src/HeaderAndTagline/TaglineSection/Tagline.tsx
+++ src/HeaderAndTagline/TaglineSection/Tagline.tsx
@@ -3,7 +3,10 @@ import "./tagline.css";
 function Tagline() {
   return (
     <div className="tagText">
-      <h3>The weather in every corner of the world.</h3>
+      <h3>
+        The weather in every corner
+        <br /> of the world.
+      </h3>
       <h5>
         Pick a place on the map and find out the weather. Plan better your trips
         or daily tasks. With Weatherable you can now feel safe, ready and
